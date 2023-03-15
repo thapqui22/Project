16e8da744e0ea6d56335915258623d8466018486
An Huynh <thapqui@gmail.com>
2023-03-15 16:19:45 +0700

    ádasd

--- src/component/carousel/CarouselProductCartSlider.jsx
+++ src/component/carousel/CarouselProductCartSlider.jsx
@@ -1,8 +1,242 @@
 import React from "react";
 import "./carouselproductcartslider.scss";
-
 const CarouselProductCartSlider = () => {
-  return <div>CarouselProductCartSlider</div>;
+  return (
+    <div class="container">
+      <div class="row my-5">
+        <h1 class="text-center">Featured Products</h1>
+        <p class="fw-light w-75 mx-auto text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est
+          neque facilis debitis, repellat dolorem.
+        </p>
+      </div>
+      <div class="row g-4 my-5 mx-auto owl-carousel owl-theme">
+        <div class="col product-item mx-auto">
+          <div class="product-img">
+            <img
+              src="https://choicacanh.com/content/uploads/cms/2020/12/14/huong-dan-cham-soc-ca-neon-vua_600x400.jpg"
+              alt=""
+              class="img-fluid d-block mx-auto"
+            />
+            <span class="heart-icon">
+              <i class="far fa-heart"></i>
+            </span>
+            <div class="row btns w-100 mx-auto text-center">
+              <button type="button" class="col-6 py-2">
+                <i class="fa fa-cart-plus"></i> Add to Cart
+              </button>
+              <button type="button" class="col-6 py-2">
+                <i class="fa fa-binoculars"></i> View
+              </button>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+
+          <div class="product-info p-3">
+            <span class="product-type">Electronics & Accessories</span>
+            <a
+              href="#"
+              class="d-block text-dark text-decoration-none py-2 product-name"
+            >
+              Apple - Apple Watch Series 3 White Sports Band
+            </a>
+            <span class="product-price">$ 100.50</span>
+            <div class="rating d-flex mt-1">
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>
+                <i class="fa fa-star"></i>
+              </span>
+              <span>(25 reviews)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CarouselProductCartSlider;
